2f9e213a5e3f977e2ce28ba62eebaf0db9b316ea
Ben Homan <ben@benhoman.com>
2024-02-22 21:45:23 -0500

    fix fidget

--- lua/bhoman/plugins/colors.lua
+++ lua/bhoman/plugins/colors.lua
@@ -1,23 +1,27 @@
-function ColorMyPencils(color)
-    color = color or "rose-pine"
-    vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("rose-pine").setup({
-            disable_background = true,
-        })
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("tokyonight-night")
 
-        vim.cmd("colorscheme rose-pine")
+			-- You can configure highlights by doing something like
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		enabled = false,
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+			})
 
-        ColorMyPencils()
-    end
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
 }
--- lua/bhoman/plugins/lsp.lua
+++ lua/bhoman/plugins/lsp.lua
@@ -3,10 +3,21 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
-		{ "j-hui/fidget.nvim", opts = {} },
-		"folke/neodev.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
+		-- extras
+		"folke/neodev.nvim",
+		{
+			"j-hui/fidget.nvim",
+			opts = {},
+			-- opts = {
+			-- 	notification = {
+			-- 		window = { winblend = 0 },
+			-- 	},
+			-- },
+		},
+
+		-- dependencies
 		"hrsh7th/cmp-nvim-lsp",
 		"nvim-telescope/telescope.nvim",
 	},
@@ -14,84 +25,77 @@ return {
 	config = function()
 		require("neodev").setup()
 
-		--  This function gets run when an LSP connects to a particular buffer.
-		local on_attach = function(_, bufnr)
-			-- We create a function that lets us more easily define mappings specific
-			-- for LSP related items. It sets the mode, buffer and description for us each time.
-			local nmap = function(keys, func, desc)
-				vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
-			end
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("custom-lsp-attach", { clear = true }),
+			callback = function(event)
+				local map = function(keys, func, desc)
+					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				end
 
-			-- Important LSP Navigation keybinds
-			--
-			-- Jump to the definition of the word under your cursor.
-			--  To jump back, press <C-T>.
-			local builtin = require("telescope.builtin")
-			nmap("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
-			nmap("gr", builtin.lsp_references, "[G]oto [R]eferences")
-			nmap("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
-			nmap("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
-			nmap("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
-			nmap("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+				local builtin = require("telescope.builtin")
 
-			-- NOTE: This is not Goto Definition, this is Goto Declaration.
-			--  For example, in C this would take you to the header
-			nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+				-- Important LSP Navigation keybinds
+				--
+				-- Jump to the definition of the word under your cursor.
+				--  To jump back, press <C-T>.
+				map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
+				map("gr", builtin.lsp_references, "[G]oto [R]eferences")
+				map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+				map("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
+				map("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+				map("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-			-- Rename the variable under your cursor
-			nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-			-- Execute a code action, usually your cursor needs to be on top of an error
-			-- or a suggestion from your LSP for this to activate.
-			nmap("<leader>ca", function()
-				vim.lsp.buf.code_action({ context = { only = { "quickfix", "refactor", "source" } } })
-			end, "[C]ode [A]ction")
+				-- Rename the variable under your cursor
+				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
-			-- See `:help K` for why this keymap
-			nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+				-- Execute a code action, usually your cursor needs to be on top of an error
+				-- or a suggestion from your LSP for this to activate.
+				map("<leader>ca", function()
+					vim.lsp.buf.code_action({ context = { only = { "quickfix", "refactor", "source" } } })
+				end, "[C]ode [A]ction")
 
-			-- Show the signature of the function you're currently completing.
-			nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-		end
+				-- See `:help K` for why this keymap
+				map("K", vim.lsp.buf.hover, "Hover Documentation")
 
-		-- LSP servers and clients are able to communicate to each other what features they support.
-		--  By default, Neovim doesn't support everything that is in the LSP Specification.
-		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-		--  So, we create new capabilties with nvim cmp, and then broadcast that to the servers.
+				-- Show the signature of the function you"re currently completing.
+				map("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+			end,
+		})
+
 		local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-		-- Enable the following language servers
 		local servers = {
-			-- clangd = {},
+			clangd = {},
 			gopls = {},
 			pyright = {},
 			rust_analyzer = {},
-			tsserver = {},
 			html = { filetypes = { "html", "twig", "hbs" } },
-
-			jsonls = {},
+			tsserver = {},
 
 			lua_ls = {
-				-- cmd = {...},
-				-- filetypes { ...},
-				-- capabilities = {},
 				settings = {
 					Lua = {
 						workspace = { checkThirdParty = false },
 						telemetry = { enable = false },
-						-- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-						-- diagnostics = { disable = { 'missing-fields' } },
+						-- You can toggle below to ignore Lua_LS"s noisy `missing-fields` warnings
+						diagnostics = { disable = { "missing-fields" } },
 					},
 				},
 			},
 		}
 
-		-- Ensure the servers above are installed
-		require("mason").setup()
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+			"stylua",
+			"jsonls",
+			"pyright",
+			"ruff",
+		})
 
-		local installed = { "jsonls", "pyright", "stylua", "ruff" }
-		vim.list_extend(installed, vim.tbl_keys(servers))
-		require("mason-tool-installer").setup({ ensure_installed = installed })
+		require("mason").setup()
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 		require("mason-lspconfig").setup({
 			handlers = {
 				function(server_name)
@@ -100,10 +104,7 @@ return {
 						cmd = server.cmd,
 						settings = server.settings,
 						filetypes = server.filetypes,
-						on_attach = on_attach,
-						-- TODO: Think about what we wanna do here.
-						-- capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities),
-						capabilities = server.capabilities or capabilities,
+						capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {}),
 					})
 				end,
 			},
