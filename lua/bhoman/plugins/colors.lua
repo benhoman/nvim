610d71bfe35811e816a92aaaf3ade9db37fe83eb
Ben Homan <bhoman@consumeraffairs.com>
2024-05-02 15:51:43 -0400

    add cyberdream

--- lua/bhoman/plugins/colors.lua
+++ lua/bhoman/plugins/colors.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"folke/tokyonight.nvim",
+		enabled = false,
 		lazy = false,
 		priority = 1000,
 		config = function()
@@ -22,6 +23,29 @@ return {
 			})
 
 			vim.cmd("colorscheme rose-pine")
+		end,
+	},
+	{
+		"scottmckendry/cyberdream.nvim",
+		enabled = true,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				-- Recommended - see "Configuring" below for more config options
+				transparent = true,
+				italic_comments = true,
+				hide_fillchars = true,
+				borderless_telescope = true,
+				terminal_colors = true,
+			})
+			vim.cmd("colorscheme cyberdream") -- set the colorscheme
+			local cyberdream = require("lualine.themes.cyberdream")
+			require("lualine").setup({
+				options = {
+					theme = "cyberdream",
+				},
+			})
 		end,
 	},
 }
